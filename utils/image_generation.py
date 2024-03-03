d0cb414c6072efa0506183178d5764a0e44f3041
annu12340 <annujolly17@gmail.com>
2024-03-03 20:29:35 +0530

    Working on virtual env

--- utils/image_generation.py
+++ utils/image_generation.py
@@ -1,15 +1,9 @@
-import os
 from openai import OpenAI
 client = OpenAI()
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-
-if openai_api_key is None:
-    raise ValueError("Please set the OPENAI_API_KEY environment variable")
-
 response = client.images.generate(
   model="dall-e-3",
-  prompt="a white siamese cat",
+  prompt="Good morning message",
   size="1024x1024",
   quality="standard",
   n=1,

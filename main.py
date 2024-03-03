b1d117a46a59ffcbc2180d7d7004bfe3ca408b73
annu12340 <annujolly17@gmail.com>
2024-03-03 22:10:52 +0530

    Working on the frontend

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from steganography.encode import encode
 from utils.image_generation import  generate_new_image
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome page"
 
-@app.route('/generate', methods=['GET', 'POST'])
-def generate_image():
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
     if request.method == 'POST':
         message = request.form['message']
-        generate_new_image()
+        # generate_new_image()
         encode("output", message)
         return render_template('result.html', message=message)
     return render_template('index.html')

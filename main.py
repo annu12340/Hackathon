4e130cc5813d965250894607900c1e2154d93ebe
annu12340 <annujolly17@gmail.com>
2024-03-04 23:21:09 +0530

    Working on the frontend of the site

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request
-from steganography.encode import encode
-from utils.image_generation import  generate_new_image
+from message_encoding.steganography.encode import encode
+from message_encoding.utils.image_generation import  generate_new_image
 app = Flask(__name__)
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    return render_template('home.html')
+
+@app.route('/generate', methods=['GET', 'POST'])
+def generate_secret_message_encoding():
     if request.method == 'POST':
         message = request.form['message']
         # generate_new_image()

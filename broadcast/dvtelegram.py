24d1e094b601c811617f79e93a911d03a522a51d
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2024-03-06 03:11:41 -0400

    chore: cleanup

--- broadcast/dvtelegram.py
+++ broadcast/dvtelegram.py
@@ -4,8 +4,6 @@ from io import BytesIO
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 
-group_chat_id = -1002111911039
-personal_chat_id = -1
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,24 +18,23 @@ async def msgHandler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(update.message.text)
     
 async def photoHandler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.chat.id == group_chat_id:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Image detected!")
-        
-        # Steganography
-        if update.message.photo:
-            print('photoooo')
-            # Get the file ID of the image
-            file_id = update.message.photo[-1].file_id
-            # Get the image file
-            image_file = await context.bot.get_file(file_id)
-            # Download the image
-            image_bytes = await image_file.download_as_bytearray()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Image detected!")
+    
+    # Steganography
+    if update.message.photo:
+        print('photoooo')
+        # Get the file ID of the image
+        file_id = update.message.photo[-1].file_id
+        # Get the image file
+        image_file = await context.bot.get_file(file_id)
+        # Download the image
+        image_bytes = await image_file.download_as_bytearray()
 
-            # Perform steganography
-            message = extract_message_from_image(image_bytes)
+        # Perform steganography
+        message = extract_message_from_image(image_bytes)
 
-            # Send the extracted message as a response
-            await context.bot.send_message(chat_id=update.message.chat_id, text=message)
+        # Send the extracted message as a response
+        await context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
 def extract_message_from_image(image_bytes):
     default_message = "Name: Lydia Davis, Phone: 1234567, Please help me. I am at the SW library"
---
+++ broadcast/readme.md
@@ -0,0 +1,3 @@
+# Telegram Module 
+
+Bot to listen to messages and capture images for steganography
